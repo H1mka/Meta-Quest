8d1f18447f1ac407a1c6a6c089082d0ea420cf1c
Adelev <kak-dela1488@ukr.net>
2023-07-31 08:55:58 +0300

    Slider-fix

--- src/components/Features/Features.tsx
+++ src/components/Features/Features.tsx
@@ -1,10 +1,14 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import s from './styles.module.scss';
 import Slider from 'react-slick';
-import SliderItem from './SliderItem';
-import imgSrc from 'assets/images/features_vrHeadSet.png';
+import SliderItem from './SliderItem/SliderItem';
+import imgHeadSet from 'assets/images/features_vrHeadSet.png';
+import imgController from 'assets/images/features_controller.png';
+import imgLenses from 'assets/images/features_lenses.png';
 
 const Features: FC = () => {
+    const [currentSlide, setCurrentSlide] = useState<number>(0);
+
     const settings = {
         dots: false,
         arrows: false,
@@ -16,31 +20,37 @@ const Features: FC = () => {
         vertical: true,
         verticalSwiping: true,
         autoplay: true,
-        autoplaySpeed: 4500,
+        autoplaySpeed: 4000,
         pauseOnFocus: true,
+        beforeChange: (current: number, next: number) => setCurrentSlide(next),
     };
     return (
         <div className={s.featuresWrapper} id='features'>
+            <div className={s.slider_dots}>
+                <div className={currentSlide === 0 ? s.active : ''}></div>
+                <div className={currentSlide === 1 ? s.active : ''}></div>
+                <div className={currentSlide === 2 ? s.active : ''}></div>
+            </div>
             <Slider {...settings}>
                 <SliderItem
-                    imgSrc={imgSrc}
+                    imgSrc={imgHeadSet}
                     caption={'Key Features'}
                     text={
                         'Everything from multiplayer games to 360° videos look beyond incredible, with 20 pixels per degree visuals and a Fast-Switch LCD Display spanning 1832 x 1920 pixels per eye'
                     }
                 />
                 <SliderItem
-                    imgSrc={imgSrc}
-                    caption={'Slide 2 Feature'}
+                    imgSrc={imgController}
+                    caption={'Controller'}
                     text={
-                        'SLIDE 2 SLIDE 2 SLIDE 2 SLIDE 2 SLIDE 2 SLIDE 2 SLIDE 2 SLIDE 2 SLIDE 2 '
+                        'Every controller has wrist straps and haptic feedback. This provides physical feedback to your hand movements to help reduce strain and prevent accidental drops.'
                     }
                 />
                 <SliderItem
-                    imgSrc={imgSrc}
-                    caption={'Slide 3 Feature'}
+                    imgSrc={imgLenses}
+                    caption={'Lenses'}
                     text={
-                        'SLIDE 3 SLIDE 3 SLIDE 3 SLIDE 3 SLIDE 3 SLIDE 3 SLIDE 3 SLIDE 3 SLIDE 3 '
+                        'The soft strap is designed to offer lightweight comfort for any type of player. Simply tighten or loosen the side straps and top strap for a more balanced feel and clearer picture.'
                     }
                 />
             </Slider>
---
+++ src/components/Features/SliderItem/SliderItem.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import s from './SliderItem.module.scss';
+
+interface ISliderItem {
+    imgSrc: string;
+    caption: string;
+    text: string;
+}
+
+const SliderItem: FC<ISliderItem> = ({ imgSrc, caption, text }) => {
+    return (
+        <section className={s.slider_item}>
+            <div className={s.pictureWrap}>
+                <img src={imgSrc} alt='slider_image' />
+            </div>
+            <div className={s.infoWrap}>
+                <h2>{caption}</h2>
+                <p>{text}</p>
+            </div>
+        </section>
+    );
+};
+
+export default SliderItem;
