9e5cdb85febe7f7344a67ce76ab64a219d5fb2d3
Adelev <kak-dela1488@ukr.net>
2023-08-03 20:09:55 +0300

    responsibility

--- src/components/DiveInMeta/DiveInMeta.tsx
+++ src/components/DiveInMeta/DiveInMeta.tsx
@@ -9,10 +9,6 @@ const DiveInMeta: FC = () => {
             <h1>Dive in Metaverse!</h1>
             <CustomButton text='Buy now' padding='26px 52px' color='var(--textLight)' />
             <img src={img} alt='Meta Quest 2' />
-            {/* <div className={s.textWrapper}>
-                <p>The best way to experience virtual reality!</p>
-                <h1>Meta Quest 2</h1>
-            </div> */}
         </article>
     );
 };
